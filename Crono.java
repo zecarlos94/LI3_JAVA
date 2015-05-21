a8ea174ef7d3e303fd9cdab3fbb3c1960778d0c7
zecarlos94 <zecarlos@MBP-de-Jose-4.home>
2015-05-21 17:10:30 +0100

    Acrescentei tudo o k tenho;fiz novamente a leitura;os catálogos tenho de os voltar a fazer

---
+++ Crono.java
@@ -0,0 +1,31 @@
+/**
+ * Crono = mede um tempo entre start() e stop()
+ * O tempo é medido em nanosegundos e convertido para 
+ *  um double que representa os segs na sua parte inteira.
+ * 
+ * @author FMM 
+ * @version 1.0 / 2006
+ */
+public class Crono {
+
+  private static long inicio = 0L;
+  private static long fim = 0L;
+  
+/** inicia a contagem de tempo */
+  public static void start() { 
+      fim = 0L; inicio = System.nanoTime();  
+  }
+  
+/** termina a contagem de tempo  e devolve diferença em segs */
+  public static double stop() { 
+      fim = System.nanoTime();
+      long elapsedTime = fim - inicio;
+      double segs = elapsedTime / 1.0E09;
+      return segs;
+  }
+  
+/** converte a diferença numa string */
+  public static String print() {
+      return "" + stop();
+  }
+}
---
+++ Hipermercado.java
@@ -0,0 +1,28 @@
+import static java.lang.System.*;
+
+/**
+ * Escreva a descrição da classe Hipermercado aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class Hipermercado
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+  
+void menu () {
+        out.print ("\n\n--------------------------------MENU--------------------------------------------------------------------------------\n\n");
+        out.print ("1 - CONSULTAS ESTATISTICAS\n");
+        out.print ("2 - LISTA ORDENADA COM CÓDIGOS DOS PRODUTOS NUNCA COMPRADOS E O RESPECTIVO TOTAL\n");
+        out.print ("3 - LISTA ORDENADA COM CÓDIGOS DOS CLIENTES NUNCA COMPRADOS E O RESPECTIVO TOTAL\n");
+        out.print ("4 - NÚMERO TOTAL COMPRAS E TOTAL DE CLIENTES NUM MÊS\n");
+        out.print ("5 - NÚMERO DE COMPRAS, PRODUTOS E O TOTAL GASTO POR MÊS \n");
+        out.print ("6 - NÚMERO DE VEZES QUE PRODUTO FOI COMPRADO POR MÊS E O NÚMERO DE CLIENTES QUE O FIZERAM E O TOTAL FACTURADO\n");
+        out.print ("7 - NÚMERO DE VEZES QUE PRODUTO FOI COMPRADO EM MODO N OU P E AS RESPECTIVAS FACTURAÇÕES\n");
+        out.print ("8 - LISTA DE CÓDIGOS PRODUTOS QUE UM CLIENTE MAIS COMPROU \n");
+        out.print ("9 - CONJUNTO DE X PRODUTOS MAIS VENDIDOS EM TODO O ANO E O NÚMERO DE CLIENTES QUE OS COMPRARAM\n");
+        out.print ("10 -DETERMINAR OS X CLIENTES QUE COMPRARAM MAIOR NÚMERO DE PRODUTOS DIFERENTES\n");
+        out.print ("11 -CONJUNTO DE X CLIENTES QUE MAIS COMPRARAM UM PRODUTO E O VALOR GASTO NELE\n");
+        out.print ("---------------------------------------------------------------------------------------------------------------------\n\n");
+ }
+}
